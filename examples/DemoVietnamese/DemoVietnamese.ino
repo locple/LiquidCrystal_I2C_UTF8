a208fad7cad3303c67de2dd2864815d61925808c
locple <131681238+locple@users.noreply.github.com>
2024-03-22 00:26:24 +0700

    Update DemoVietnamese.ino

--- examples/DemoVietnamese/DemoVietnamese.ino
+++ examples/DemoVietnamese/DemoVietnamese.ino
@@ -3,25 +3,13 @@
   a Vietnamese text and clear the screen before printing another text.
   Author: Le Phuoc Loc <phuocloc@gmail.com>
 
-  Circuit:
-  - Arduino Uno/Mini/Nano:  A5 (SCL),   A4 (SDA)
-  - Arduino Micro:          3 (SCL),    2 (SDA)
-  - LOLIN D1 Mini:          5 (SCL),    4 (SDA)
-  - ESP8266:                D1 (SCL),   D2 (SDA)
-  - LOLIN D32:              22 (SCL),   21 (SDA)
-  - ESP32:                  D22 (SCL),  D21 (SDA)
-
-  Coding: steps to follow
+  Note:
   - Include LCDI2C_Viet.h for the main class and customized characters for Vietnamese
     => All Vietnamese letters and degree (°) symbol can be printed.
   - Include ROM_Standard_JP.h for Greek symbol mappings on LCD2004A Japanese ROM
     => All English letters, sign(÷√∞→←), Greek(ΣΩαβδεθμπρ), currency(¥) symbols can be printed.
-  - Create LiquidCrystal_I2C_Viet object with I2C address=0x27, LCD size: 20 columns, 4 rows
-  - ::begin(): Initialize the LCD
-  - ::backlight(): Turn on the LCD backlight
-  - ::print(text): print text (String or char[]) to LCD
-    If no longer room to print the next word in current line, it'll be printed in next line.
-    Note: max 8 Vietnamese letters with diacritics printed on a screen, otherwise diacritics removed.
+  - If no longer room to print the next word in current line, it'll be printed in next line.
+  - Max 8 Vietnamese letters with diacritics printed on a screen, otherwise diacritics removed.
 
   https://github.com/locple/LiquidCrystal_I2C_UTF8
 */
