f9bbed45cc4199f6db70571f2eea25739c809cff
locple <131681238+locple@users.noreply.github.com>
2024-03-22 00:23:44 +0700

    Update Demo.ino

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -3,23 +3,11 @@
   a popular LCD1602A (Japanese ROM) using I2C (PCF8574)
   Author: Le Phuoc Loc <phuocloc@gmail.com>
 
-  Circuit:
-  - Arduino Uno/Mini/Nano:  A5 (SCL),   A4 (SDA)
-  - Arduino Micro:          3 (SCL),    2 (SDA)
-  - LOLIN D1 Mini:          5 (SCL),    4 (SDA)
-  - ESP8266:                D1 (SCL),   D2 (SDA)
-  - LOLIN D32:              22 (SCL),   21 (SDA)
-  - ESP32:                  D22 (SCL),  D21 (SDA)
-
-  Coding: steps to follow
+  Note:
   - Include LCDI2C_Viet.h for the main class and customized characters for Vietnamese
     => All Vietnamese letters and degree (°) symbol can be printed.
   - Include ROM_Standard_JP.h for Greek symbol mappings on LCD1602A Japanese ROM
     => All English letters, sign(÷√∞→←), Greek(ΣΩαβδεθμπρ), currency(¥) symbols can be printed.
-  - Create LiquidCrystal_I2C_Viet oject with I2C address=0x27, LCD size: 16 columns, 2 rows
-  - ::begin(): Initialize the LCD
-  - ::backlight(): Turn on the LCD backlight
-  - ::print(text): print text (String or char[]) to LCD
 
   https://github.com/locple/LiquidCrystal_I2C_UTF8
 */
